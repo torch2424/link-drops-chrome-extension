3d9ea5637cbcd063cd6c62be5193c05abf3d44bf
Aaron <torch2424@hotmail.com>
2015-10-31 00:59:05 -0700

    Getting the html for the class ready

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Popup');
+// Restores the chrome extension code
+// stored in chrome.storage.
+function restore_options() {
+
+  // Use default value of nothing
+  chrome.storage.sync.get({
+    extensionCode: ''
+  }, function(items) {
+      //In this call back, set the code to the retrieved value
+    document.getElementById('code').value = items.extensionCode;
+  });
+
+  //if we have a code show the stuff
+
+  //if not, tell them to 
+}
+
+//Restore the options on page load
+document.addEventListener('DOMContentLoaded', restore_options);

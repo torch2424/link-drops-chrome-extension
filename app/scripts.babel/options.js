00cdd76ce52e97c60c9948b07909fd6c4e00a159
Aaron <torch2424@hotmail.com>
2015-10-30 21:42:29 -0700

    Finished the options page

--- app/scripts.babel/options.js
+++ app/scripts.babel/options.js
@@ -1,5 +1,37 @@
 'use strict';
 
+// Saves options to chrome.storage
 function saveCode() {
+    //Get the code value
+    var code = document.getElementById('code').value;
 
+    //Set the code in the options
+    chrome.storage.sync.set({
+      extensionCode: code
+    }, function() {
+
+      // Update status to let user know options were saved.
+      //Timeout the div after it is displayed
+      var status = document.getElementById('status');
+      status.textContent = 'Code saved!';
+      setTimeout(function() {
+        status.textContent = '';
+      }, 750);
+    });
 }
+
+// Restores the chrome extension code
+// stored in chrome.storage.
+function restore_options() {
+
+  // Use default value of nothing
+  chrome.storage.sync.get({
+    extensionCode: ''
+  }, function(items) {
+      //In this call back, set the code to the retrieved value
+    document.getElementById('code').value = items.extensionCode;
+  });
+}
+
+//Restore the options on page load
+document.addEventListener('DOMContentLoaded', restore_options);
